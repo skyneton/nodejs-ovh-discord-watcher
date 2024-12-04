40953aafd5e220d5cb12f9d1b6512f17e378c3e6
skyneton <donginbae30@gmail.com>
2024-12-05 00:28:06 +0900

    bug fix

--- checker.js
+++ checker.js
@@ -28,52 +28,60 @@ export class Checker {
         clearInterval(this.id);
     }
 
-    start(interval) {
-        this.id = setInterval(async () => {
-            const delServerList = [];
-            for (const serverKey in this.data) {
-                const server = this.data[serverKey];
-                const regionList = new Set(Object.keys(server));
-                if (regionList.size == 0) {
-                    delServerList.add(serverKey);
+    async work() {
+        const delServerList = [];
+        for (const serverKey in this.data) {
+            const server = this.data[serverKey];
+            const regionList = new Set(Object.keys(server));
+            if (regionList.size == 0) {
+                delServerList.add(serverKey);
+                continue;
+            }
+            if (regionList.has("eur") && regionList.size > 1) {
+                regionList.add("gra");
+                regionList.add("sbg");
+                regionList.add("rbx");
+                regionList.add("bhs");
+                regionList.add("waw");
+                regionList.add("fra");
+                regionList.add("lon");
+                regionList.delete("eur");
+            }
+            const delRegionList = [];
+            const availabilities = await getAvailable(serverKey, Array.from(regionList).join(','));
+            for (const regionKey in server) {
+                if (regionKey.size == 0) delRegionList.push(regionKey);
+                if (regionKey == 'eur') {
+                    let eur = "";
+                    if (availabilities.has("gra")) eur += ",gra";
+                    if (availabilities.has("sbg")) eur += ",sbg";
+                    if (availabilities.has("rbx")) eur += ",rbx";
+                    if (availabilities.has("bhs")) eur += ",bhs";
+                    if (availabilities.has("waw")) eur += ",waw";
+                    if (availabilities.has("fra")) eur += ",fra";
+                    if (availabilities.has("lon")) eur += ",lon";
+                    if(eur)
+                        this.sendChannels(serverKey, server[regionKey], eur.substring(1));
+
                     continue;
                 }
-                if (regionList.has("eur") && regionList.size > 1) {
-                    regionList.add("gra");
-                    regionList.add("sbg");
-                    regionList.add("rbx");
-                    regionList.add("bhs");
-                    regionList.add("waw");
-                    regionList.add("fra");
-                    regionList.add("lon");
-                    regionList.delete("eur");
-                }
-                const delRegionList = [];
-                const availabilities = await getAvailable(serverKey, Array.from(regionList).join(','));
-                for (const regionKey in server) {
-                    if (regionKey.size == 0) delRegionList.push(regionKey);
-                    if (regionKey == 'eur') {
-                        const eur = "";
-                        if (availabilities.has("gra")) eur += ",gra";
-                        if (availabilities.has("sbg")) eur += ",sbg";
-                        if (availabilities.has("rbx")) eur += ",rbx";
-                        if (availabilities.has("bhs")) eur += ",bhs";
-                        if (availabilities.has("waw")) eur += ",waw";
-                        if (availabilities.has("fra")) eur += ",fra";
-                        if (availabilities.has("lon")) eur += ",lon";
-                        if(eur)
-                            this.sendChannels(serverKey, server[regionKey], eur.substring(1));
-
-                        continue;
-                    }
-                    if (availabilities.has(regionKey)) this.sendChannels(serverKey, server[regionKey], regionKey);
-                }
-                for (const region of delRegionList) {
-                    delete server[region];
-                }
+                if (availabilities.has(regionKey)) this.sendChannels(serverKey, server[regionKey], regionKey);
             }
-            for (const server of delServerList) {
-                delete this.data[server];
+            for (const region of delRegionList) {
+                delete server[region];
+            }
+        }
+        for (const server of delServerList) {
+            delete this.data[server];
+        }
+    }
+
+    start(interval) {
+        this.id = setInterval(async () => {
+            try {
+                await work();
+            } catch (e) {
+                console.error(e);
             }
         }, interval * 1000);
     }
